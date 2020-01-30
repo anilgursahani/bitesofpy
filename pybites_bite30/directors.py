90a54316cb0883dda24aab969047078896beb5c6
Anil Gursahani <anilgursahani59@gmail.com>
2020-01-30 13:32:00 -0600

    Added code to directors.py to output list of directors with average score in ascending order

--- pybites_bite30/directors.py
+++ pybites_bite30/directors.py
@@ -22,12 +22,61 @@ def get_movies_by_director():
     """Extracts all movies from csv and stores them in a dict,
     where keys are directors, and values are a list of movies,
     use the defined Movie namedtuple"""
+    
+    (filename,header) =  urlretrieve(remote, local)
+    with open(filename, 'r') as csvFile:
+        dictData = DictReader(csvFile)
+        moviesByDirector = {}
+        
+        for line in dictData:
+            Movie = namedtuple('Movie', 'title year score')
+        
+            director = line['director_name']
+            Movie.title = line['movie_title']
+            Movie.year = line['title_year']
+            Movie.score = line['imdb_score']
+            
+            movieYear = Movie.year
+            if movieYear.isdigit():
+                movieYear = int(movieYear)
+                if movieYear < MIN_YEAR:
+                    continue
+            else:
+                continue
+            
+        
+            if director in moviesByDirector.keys():
+           
+                listOfDirectorMovies = moviesByDirector[director]
+                listOfDirectorMovies.append(Movie)
+            
+            
+            
+            else:
+            
+                listOfDirectorMovies = []
+                listOfDirectorMovies.append(Movie)
+                moviesByDirector[director] = listOfDirectorMovies
+                
+            
+      
+    
+    return moviesByDirector
+        
+   
     pass
 
 
 def calc_mean_score(movies):
     """Helper method to calculate mean of list of Movie namedtuples,
        round the mean to 1 decimal place"""
+       
+    numMovies = len(movies)
+    sum = 0
+    for movie in movies:
+        sum = sum + float(movie.score)
+    avg = sum/numMovies
+    return(round(avg,1))
     pass
 
 
@@ -36,4 +85,52 @@ def get_average_scores(directors):
        return a list of tuples (director, average_score) ordered by highest
        score in descending order. Only take directors into account
        with >= MIN_MOVIES"""
+       
+    directorList = []   
+    for director in directors.keys():
+       
+        movieList = directors[director]
+        numMovies = len(movieList)
+        if numMovies >= MIN_MOVIES:
+            avgScore = calc_mean_score(movieList)
+            AVG_SCORE = namedtuple('AVG_SCORE', ['director', 'average_score'])
+            AVG_SCORE.director = director
+            AVG_SCORE.average_score = avgScore
+            directorList.append(AVG_SCORE)
+            
+        
+    DoSortDirectorList(directorList)
+        
+        
+    
     pass
+
+'''
+ This function sorts the list of directors that contains list of tuples (director , average_score) ordered by highest score in descending order.
+
+'''
+
+def DoSortDirectorList(theList):
+    numItems = len(theList)
+    for posToSetIdx in range(numItems-1):
+        for j in range(posToSetIdx+1,numItems):
+            posToSetItem = theList[posToSetIdx]
+            jItem = theList[j]
+            posToSetAvgScore = posToSetItem.average_score
+            jItemAvgScore = jItem.average_score
+            if jItemAvgScore > posToSetAvgScore:
+                temp = theList[posToSetIdx]
+                theList[posToSetIdx] = theList[j]
+                theList[j] = temp
+            
+            
+    
+    
+def main():
+    directors = get_movies_by_director()
+    get_average_scores(directors)
+    
+if __name__ == '__main__':
+    main()
+    
+    
