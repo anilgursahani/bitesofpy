2eb05941fd615c55b4cefa182e746a760d59b041
Anil Gursahani <anilgursahani59@gmail.com>
2020-03-21 20:37:24 -0500

    submission Bite 143 @ codechalleng.es

---
+++ 143/merge.py
@@ -0,0 +1,29 @@
+NOT_FOUND = "Not found"
+
+group1 = {'tim': 30, 'bob': 17, 'ana': 24}
+group2 = {'ana': 26, 'thomas': 64, 'helen': 26}
+group3 = {'brenda': 17, 'otto': 44, 'thomas': 46}
+
+
+def get_person_age(name):
+    """Look up name (case insensitive search) and return age.
+       If name in > 1 dict, return the match of the group with
+       greatest N (so group3 > group2 > group1)
+    """
+    age = NOT_FOUND
+    
+    if isinstance(name, str):
+        
+        name = name.lower()
+        listOfGroups = [group3,group2, group1]
+            
+        for group in listOfGroups:
+            if name in group:
+                age = group[name]
+                break
+    return age
+    
+    
+    pass
+
+
